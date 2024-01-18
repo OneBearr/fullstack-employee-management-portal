2bed889116b6f4ba807a9db31efab6a3ebf06042
Xiaofan Lin <xiaofanlinus@gmail.com>
2024-01-17 20:36:18 -0800

    finish backend basic files setup issue05

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+
+const connectDB = require('./db');
+const errorHandlerMiddleware = require('./middlewares/errorHandler');
+const app = express();
+const port = 3000;
+
+connectDB();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+app.use(errorHandlerMiddleware);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
